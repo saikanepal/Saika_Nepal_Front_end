2d52e970e6fce9e582315d0ed5b6090804a02fd1
skarn3 <skarn@emich.edu>
2024-03-05 15:21:38 -0500

    homepage animations

--- src/Components/AboutSaikaPage.js
+++ src/Components/AboutSaikaPage.js
@@ -1,38 +1,53 @@
 import React from "react";
 import { ReactTyped } from "react-typed";
-
-const AboutSaikaPage = ({contactRef}) => {
+import Lottie from "lottie-react";
+import laptopAnimation from "../assets/laptopAnimation.json";
+import candle from "../assets/candle.json";
+const AboutSaikaPage = ({ contactRef }) => {
   return (
     <div className="text-white bg-[#000300] ">
-      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+
+      <div className=" px-4 gap-2  max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <div className=" pt-5 flex justify-center items-center">
+          <Lottie onClick={ () => {
+            contactRef.current.scrollIntoView({ behavior: "smooth" })
+          }}  animationData={laptopAnimation} />
+        </div>
         <p className="text-[#b54b9f] font-bold p-2">
           A Nepal Based Startup
         </p>
-        <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold md:py-3">
-          Ignite Success with us: Your Digital Spark!
-        </h1>
-        <div className="flex justify-center items-center">
-          <p className="md:text-3xl sm:text-xl text-lg font-bold py-4">
-            Scalable Solutions for
-          </p>
+        <div className=" flex flex-row">
+          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold md:py-3">
+            Ignite Success With Us: Your Digital Spark
+          </h1>
+          <Lottie onClick={ () => {
+            contactRef.current.scrollIntoView({ behavior: "smooth" })
+          }} className="  w-14 h-30" animationData={candle} />
+        </div>
+        <div className="mb-3 flex justify-center items-center">
+
           <ReactTyped
-            className='md:text-3xl sm:text-xl text-lg font-bold md:pl-4 pl-2'
-            strings={['Web Applications', 'Mobile Applications', 'Database Management', 'Deployment', 'Maintenance', 'Upgrades']}
-            typeSpeed={120}
-            backSpeed={140}
+            className='text-[#FFF6E9] font-serif text-3xl md:text-4xl lg:text-5xl font-bold'
+            strings={['Your Website', 'Your Software', 'Your Vision', 'Our Expertise']}
+            typeSpeed={140}
+            backSpeed={130}
             loop
+            // Additional props for better customization
+            showCursor={true}
+            cursorChar="|"
+            smartBackspace={true}
           />
         </div>
         <div className="max-w-[800px] mx-auto text-center">
           <p className="md:text-lg text-base font-bold text-gray-400">
 
-            Saika Nepal is your go-to startup for all things web-related. From creating user-friendly websites/webapps to streamlining data management, we're here to simplify your online presence. With us, innovation meets simplicity, ensuring your digital journey is smooth and successful. Join us at Saika Nepal and let's make your web dreams a reality.
+            Your Web Professionals! Simplifying your online presence, from websites to data management. Let's ensure your web journey is seamless – join us today!.
           </p>
         </div>
         <button onClick={
-            ()=>{
+          () => {
             contactRef.current.scrollIntoView({ behavior: "smooth" })
-            }
+          }
         } className="bg-[#b54b9f] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">
           Get Started
         </button>
