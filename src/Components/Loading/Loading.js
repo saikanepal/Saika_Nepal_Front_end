9931af111ddabc2ebe138bab8796a03398115525
skarn3 <skarn@emich.edu>
2024-03-09 16:00:48 -0500

    loading effects

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Lottie from "lottie-react";
+import loadingAnimation from "../../assets/loading.json";
 
 const Loading = ({ message = "Loading..." }) => {
   return (
     <div className="px-3 py-1 text-xs font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-full animate-pulse dark:bg-blue-900 dark:text-blue-200">
-      {message}
+      <Lottie
+
+        animationData={loadingAnimation}
+
+      />
     </div>
   );
 };
